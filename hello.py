87991db3b18b43ee459a0de2f2b3439d28661d72
HuberyChang <1632425633@qq.com>
2017-06-18 19:57:10 +0800

    world

--- hello.py
+++ hello.py
@@ -3,4 +3,9 @@ print("add coding:utf-8")
 =======
 print("add /usr/bin/env")
 >>>>>>> b852a6c43463f5c0993ca0cf33efe861590f5a44
+<<<<<<< HEAD
 print("add coding:utf-8")
+=======
+print("add /usr/bin/env")
+>>>>>>> e36012da6005ab3913c7df5e2c563d7244970f7f
+pring("world")
